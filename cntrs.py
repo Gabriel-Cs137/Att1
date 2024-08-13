57f69bcf9bda30cefe660451f8e1c8e3f73a8103
Luis Fernando <luisvirissimo789@gmail.com>
2024-08-13 11:01:39 -0400

    Oopsie doopsie
    hAHAHA

--- cntrs.py
+++ cntrs.py
@@ -3,3 +3,5 @@
 # Que tomate seco que ele era.
 
 # Mas mesmo em sua casca seca, a vida germinou.
+
+# AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH - gritou a vida dentro do tomate. Pois só via vermelho.
