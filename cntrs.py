10cad2f731e35f34f95e1810fd4deab852af25c7
Gabriel-Cs137 <gabrielpaganotti176@gmail.com>
2024-08-15 07:28:55 -0400

    Comitando comitagens comitadas
    Batata frita é muito bom, mas mousse de maracujá é melhor.

--- cntrs.py
+++ cntrs.py
@@ -7,3 +7,5 @@
 # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH - gritou a vida dentro do tomate. Pois só via vermelho.
 
 # Mas ela conseguiu superar seu medo do vermelho, e logo após isso ela disse : Hehehe subi direto pra main
+
+# Após a superação de seu terrivel medo de vermelho, a vida contemplou algo ainda mais assustador, o vazio imensuravel que rondava o tomate em que ela habitava
