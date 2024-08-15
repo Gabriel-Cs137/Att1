9a11358831480c7c2ba3371218804b65a4d96689
Luis Fernando <luisvirissimo789@gmail.com>
2024-08-15 07:26:59 -0400

    Mudei a main kkkk

--- cntrs.py
+++ cntrs.py
@@ -5,3 +5,5 @@
 # Mas mesmo em sua casca seca, a vida germinou.
 
 # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH - gritou a vida dentro do tomate. Pois só via vermelho.
+
+# Mas ela conseguiu superar seu medo do vermelho, e logo após isso ela disse : Hehehe subi direto pra main
